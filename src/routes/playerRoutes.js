fc8779206970a59ad311f540a37128f450027511
geokoko <geokoko3@outlook.com.gr>
2025-07-16 05:04:36 +0300

    Deleted monolith approach

--- src/routes/playerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const playerController = require('../controllers/playerController');
-
-// Player views their own stats in a specific tournament
-router.get('/:userId/:tournamentId/stats', playerController.getPlayerStats);
-
-module.exports = router;
