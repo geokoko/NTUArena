781a0f451553052f5db0751d0426bad168bb5273
geokoko <geokoko3@outlook.com.gr>
2025-10-01 06:11:20 +0300

    Updated docker-compose.yml

--- api/src/routes/pairing.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/pairingController');
-
-router.get('/generate/:tournamentId', controller.generate.bind(controller));
-
-module.exports = router;
-
