5755eedbb8148627f6d14f746a896f750e09c9f6
geokoko <geokoko3@outlook.com.gr>
2025-10-08 01:32:08 +0300

    Add defaults and references in schemas correctly according to each use case

--- backend/src/models/Game.js
+++ backend/src/models/Game.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const GameSchema = new mongoose.Schema({
-	playerWhite: { type: mongoose.Schema.Types.ObjectId, required: true },
-	playerBlack: { type: mongoose.Schema.Types.ObjectId, required: true },
-	tournament: { type: mongoose.Schema.Types.ObjectId, required: true },
+	playerWhite: { type: mongoose.Schema.Types.ObjectId, ref: 'Player', required: true },
+	playerBlack: { type: mongoose.Schema.Types.ObjectId, ref: 'Player', required: true },
+	tournament: { type: mongoose.Schema.Types.ObjectId, ref: 'Tournament', required: true },
 	isFinished: { type: Boolean, default: false },
 	finishedAt: { type: Date },
 	resultColor: { type: String, enum: ['white', 'draw', 'black'] },
--- backend/src/models/Player.js
+++ backend/src/models/Player.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose');
 
 const PlayerSchema = new mongoose.Schema({
-	user: { type: mongoose.Schema.Types.ObjectId, required: true },
-	tournament: { type: mongoose.Schema.Types.ObjectId, required: true },
+	user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+	tournament: { type: mongoose.Schema.Types.ObjectId, ref: 'Tournament', required: true },
 	score: { type: Number, required: true, default: 0 },
-	liveRating: { type: Number, required: true },
+	liveRating: { type: Number, required: true, default: 1200 },
 	isPlaying: { type: Boolean, default: false },
 	waitingSince: { type: Date, default: Date.now },
-	gameHistory: [{ type: mongoose.Schema.Types.ObjectId }],
+	gameHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Game' }],
 	colorHistory: [{ type: String, enum: ['white', 'black'] }],
 	recentOpponents: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }]
 });
 
 module.exports = mongoose.model('Player', PlayerSchema);
-
