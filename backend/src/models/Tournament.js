90ff9c43388c3a36695dc6bd45ffa86226f09c07
geokoko <geokoko3@outlook.com.gr>
2025-10-08 01:31:09 +0300

    Restructure tournament schema

--- backend/src/models/Tournament.js
+++ backend/src/models/Tournament.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 
 const tournamentSchema = new mongoose.Schema({
 	name: { type: String, required: true },
-	tournLocation: { type: String },
+	tournLocation: { type: String, default: '' },
 	startDate: { type: Date, required: true },
 	endDate: { type: Date, required: true },
+	description: { type: String, default: '' },
+	timeControl: { type: String, default: '' },
+	type: { type: String, default: 'arena' },
+	maxPlayers: { type: Number, default: 100 },
 	tournStatus: { type: String, enum: ['upcoming', 'in progress', 'completed'], default: 'upcoming' },
 	participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
 	scoreboard: [{
